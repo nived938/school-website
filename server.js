cbfc67497c0abfad9b01eb5028c2429af2a51128
Nived <nivedsreejaharidasan@gmail.com>
2026-07-17 20:15:13 +0530

    Update package.json

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const session = require("express-session");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const ADMIN_USERNAME = "admin";
 const ADMIN_PASSWORD = "ghsschemnad";
